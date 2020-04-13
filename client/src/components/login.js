243f469f8cd78e5cd1dcb6e4944985a023ef5ad2
Faizan Rasheed <Faizanrasheed4@gmail.com>
2020-04-13 22:41:59 +0500

    Update login.js

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { login } from "../utility/auth.service";
-import SignUp from "./register";
 
 export default class Login extends Component {
     constructor() {
