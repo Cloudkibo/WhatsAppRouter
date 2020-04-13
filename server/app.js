471b43f66dcf881cab6af472eb6a7821ba0683ef
Faizan <faizanrasheed4@mail.com>
2020-04-13 16:46:01 +0500

    droplet setup

--- server/app.js
+++ server/app.js
@@ -22,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
