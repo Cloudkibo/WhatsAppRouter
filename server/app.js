9d2b6fd5a157864f6af693a5708222096b8430b4
Faizan Rasheed <Faizanrasheed4@gmail.com>
2020-04-14 13:14:00 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mysql = require('mysql');
 const bodyParser = require('body-parser')
 
 const usersRouter = require('./api/user');
 const authRouter = require('./api/authentication/')
 const urlRouter = require('./api/urls')
 
-const app = express();
+
+var httpsApp = express()
+var httpApp = express()
+const app = (process.env.NODE_ENV === 'production') ? httpsApp : httpApp
+
+
 
 
 // view engine setup
@@ -22,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -39,9 +44,9 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('/urls', urlRouter)
-app.use('/users', usersRouter);
 app.use('/', authRouter);
+app.use('/users', usersRouter);
+app.use('/urls', urlRouter)
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
