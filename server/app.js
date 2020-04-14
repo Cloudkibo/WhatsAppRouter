d4559c8093f0c7580eff146515957f6152e4bd88
Faizan Rasheed <Faizanrasheed4@gmail.com>
2020-04-14 16:06:05 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,24 +3,26 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mysql = require('mysql');
 const bodyParser = require('body-parser')
 
 const usersRouter = require('./api/user');
 const authRouter = require('./api/authentication/')
 const urlRouter = require('./api/urls')
-const http = require('http')
-const https = require('https')
+
+const app = express();
 
 
-var httpsApp = express()
-var httpApp = express()
-const app = (process.env.NODE_ENV === 'staging') ? httpsApp : httpApp
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
