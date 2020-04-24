11b4eac922f2bb44c7373f4fe1ef460b505c3e89
Faizan <faizanrasheed4@mail.com>
2020-04-24 17:31:19 +0500

    Do not allow same urls to be add

--- server/api/urls/urls.controller.js
+++ server/api/urls/urls.controller.js
@@ -18,7 +18,6 @@ exports.get = (req, res, next) => {
                             element.redirectUrl = `/urls/redirectUrl/${element.id}`
                         }
                     })
-                    console.log(urls)
                     return config.successresponse(res, 200, 'fetched all urls successfully!', urls)
                 }
             })
@@ -63,7 +62,7 @@ exports.post = (req, res, next) => {
             connection.query(sql, data, (error, url) => {
                 if (error) {
                     connection.release()
-                    return config.errorResponse(res, 500, 'Failed to query database.', error)
+                    return config.errorResponse(res, 400, 'Failed to query database.', error.sqlMessage)
                 } else {
                     let promise = []
                     req.body.alternetUrl.forEach(element => {
@@ -83,7 +82,7 @@ exports.post = (req, res, next) => {
                         })
                         .catch(err => {
                             console.log(err)
-                            return config.errorResponse(res, 500, 'Failed to create url', err)
+                            return config.errorResponse(res, 400, 'Failed to create url', err)
                         })
                 }
             })
@@ -163,14 +162,12 @@ exports.delete = (req, res, next) => {
             connection.release()
             return config.errorResponse(res, 500, 'Failed to create database connection.', err)
         } else {
-            console.log('i am in delete')
             let sql = `SELECT * FROM urls WHERE id = ${req.body.id} OR baseUrlId = ${req.body.id}`
             connection.query(sql, (error, urls) => {
                 if (error) {
                     connection.release()
                     return config.errorResponse(res, 500, 'Failed to query database.', error)
                 } else {
-                    console.log('in urls', urls)
                     let promise = []
                     urls.forEach(element => {
                         let sql = `DELETE FROM urls WHERE id = ${element.id}`;
@@ -178,7 +175,6 @@ exports.delete = (req, res, next) => {
                     })
                     Promise.all(promise)
                         .then(result => {
-                            console.log('In Promise', result)
                             return config.successresponse(res, 200, 'Deleted url successfully!')
                         })
                         .catch(err => {
