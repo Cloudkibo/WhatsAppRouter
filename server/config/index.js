f760d34c9f0c0629aaad6badcb562fc66a30ae97
arveenkumar55 <39855662+arveenkumar55@users.noreply.github.com>
2020-06-19 10:49:15 +0500

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -11,9 +11,9 @@ exports.all = {
 exports.pool = mysql.createPool({
     connectionLimit: 100,
     host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database: 'WLB'
+    user     : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
   })
 
   exports.successresponse = (response, status, description, payload) => {
